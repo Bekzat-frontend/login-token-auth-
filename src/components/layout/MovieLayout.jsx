9ec1d7c6594fbf39b18066d6dfe3d91b1f50ad7c
Bekzat-frontend <mamateminovbekzat793@gmail.com>
2025-03-25 02:24:58 +0700

    hashed passwords with bcrypt

--- src/components/layout/MovieLayout.jsx
+++ src/components/layout/MovieLayout.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Outlet } from "react-router";
 
 function MovieLayout() {
+  const getEabledArr = async () => {
+    try {
+      const response = await fetch(URL, {
+        method: "GET",
+        headers: {
+          Authorization: `Beare ${URL}`,
+        },
+      });
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getEabledArr();
+  }, []);
   return (
     <div>
       <Outlet />
